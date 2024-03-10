82e6726e2c1d1ce872a29aba304af6b4e068af48
Sheryl L <s_lawrence0@yahoo.ca>
2024-03-10 17:24:52 -0400

    function created to write data to SVG file in index.js

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 const inquirer = require('inquirer'); // npm package
 const fs = require('fs'); // file system
 
+console.log("This is my command-line SVG Logo Maker!");
+console.log("The following questions will generate a SVG logo once answered.");
+
 // Array of questions created for user input
-// Input / questions for SVG  
+// Input / questions for SVG  log options 
+// Objects with type, name and message properties and choices for the shape options
+// When node index.js is entered in the terminal it will go through these questions for the user to answer 
 const questions = [
     {
         type: "input",
@@ -26,4 +31,14 @@ const questions = [
         name: "shape-colour",
         message: "Enter a colour for logo shape:",
     },
-]
+];
+
+// Function created to write data to SVG file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (error) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log("Complete! You can preview your SVG logo.");
+    })
+};
