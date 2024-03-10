4e628f9da211eb5ca0ae17858281e56240537119
Sheryl L <s_lawrence0@yahoo.ca>
2024-03-10 17:02:21 -0400

    user input questions in index.js and README content added

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 SVG logo maker through Node.js command-line 
 
 ## Description
-
+- The motivation for this project was to create an Object Oriented Programming application that generates a logo through the command line using node js, which can be saved as a SVG file. 
 
 ## Installation
 
--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 // Packages needed for this application 
 const inquirer = require('inquirer'); // npm package
 const fs = require('fs'); // file system
+
+// Array of questions created for user input
+// Input / questions for SVG  
+const questions = [
+    {
+        type: "input",
+        name:"text",
+        message: "Enter three characters for logo:",
+    },
+    {
+        type: "input",
+        name:"text-colour",
+        message: "Enter a colour for text:",
+    },
+    {
+        type: "input",
+        name:"shape",
+        message: "Choose a shape for logo:",
+        choices: ["Circle, Triangle, Square"],
+    },
+    {
+        type: "input",
+        name: "shape-colour",
+        message: "Enter a colour for logo shape:",
+    },
+]
