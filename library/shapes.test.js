d9362ee9fffa333755118ce5c0660ab92349ff03
Sheryl L <s_lawrence0@yahoo.ca>
2024-03-15 21:20:50 -0400

    examples folder added for svg files

--- library/shapes.test.js
+++ library/shapes.test.js
@@ -8,28 +8,28 @@ const { Circle, Triangle, Square} = require('./shapes');
 
 // Circle shape test
 describe('Circle test', () => {
-    test('test circle renders correctly' () => {
-        const shape = new Circle(),
-        shape.setColour =('blue'),
+    test('test circle renders correctly', () => {
+        const shape = new Circle()
+        shape.setColour =('blue')
         expect(shape.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="150" cy="115" r="80" fill="${this.shapeColour}"/>
-        <text x="150" y="125" font-size="70" text-anchor="middle" fill="${this.textColour}">${this.text}</text></svg>`),
+        <text x="150" y="125" font-size="70" text-anchor="middle" fill="${this.textColour}">${this.text}</text></svg>`)
     });
 });
 
 // Triangle shape test
 describe('Triangle test', () => {
-    test('test circle renders correctly' () => {
-        const shape = new Triangle(),
-        Triangle.setColour =('blue'),
-        expect(shape.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="150, 18 244, 182 56, 182" fill="${this.shapeColour}"/><text x="150" y="125" font-size="70" text-anchor="middle" fill="${this.textColour}">${this.text}</text></svg>`),
+    test('test circle renders correctly', () => {
+        const shape = new Triangle()
+        Triangle.setColour =('blue')
+        expect(shape.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="150, 18 244, 182 56, 182" fill="${this.shapeColour}"/><text x="150" y="125" font-size="70" text-anchor="middle" fill="${this.textColour}">${this.text}</text></svg>`)
     });
 });
 
 // Square shape test
 describe('Square test', () => {
-    test('test circle renders correctly' () => {
-        const shape = new Square(),
-        shape.setColour =('blue'),
-        expect(shape.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect x="73" y="40" width="160" height="160" fill="${this.shapeColour}"/><text x="150" y="125" font-size="70" text-anchor="middle" fill="${this.textColour}">${this.text}</text></svg>`),
+    test('test circle renders correctly', () => {
+        const shape = new Square()
+        shape.setColour =('blue')
+        expect(shape.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect x="73" y="40" width="160" height="160" fill="${this.shapeColour}"/><text x="150" y="125" font-size="70" text-anchor="middle" fill="${this.textColour}">${this.text}</text></svg>`)
     });
 });
